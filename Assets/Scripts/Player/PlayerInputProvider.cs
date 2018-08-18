bb1a8e14a0bd3ca603df6f17b7272d5fc9902b61
Takuma Seimiya <mj.soccer.t.s0802@gmail.com>
2018-08-18 09:41:01 +0900

    Playerの移動機構

--- Assets/Scripts/Player/PlayerInputProvider.cs
+++ Assets/Scripts/Player/PlayerInputProvider.cs
@@ -1,16 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
 
-public class PlayerInputProvider : MonoBehaviour {
+public class PlayerInputProvider : PlayerCore
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private ReactiveProperty<bool> _jump = new BoolReactiveProperty();
+    private ReactiveProperty<Vector3> _moveDirection = new ReactiveProperty<Vector3>();
+    public IReadOnlyReactiveProperty<Vector3> MoveDirection { get { return _moveDirection; } }
+    public IReadOnlyReactiveProperty<bool> JumpButton { get { return _jump; } }
+
+    protected override void OnInitialize()
+    {
+        this.UpdateAsObservable()
+                .Select(_ => Input.GetKey(KeyCode.Space))
+                .DistinctUntilChanged()
+                .Subscribe(x => _jump.Value = x);
+
+        this.UpdateAsObservable()
+            .Select(_ => new Vector3(Input.GetAxis("Horizontal"), 0, 0))
+            .Subscribe(x => _moveDirection.SetValueAndForceNotify(x));
+    }
 }
--- Assets/Scripts/Player/PlayerMover.cs
+++ Assets/Scripts/Player/PlayerMover.cs
@@ -1,16 +1,90 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UniRx;
+using UniRx.Triggers;
+using System;
 
-public class PlayerMover : MonoBehaviour {
+public class PlayerMover : PlayerCore
+{
+    [SerializeField] private float runThreshold = 1f;
+    [SerializeField] private float runSpeed = 0.1f;
+    [SerializeField] private float runForce = 50f;
+    [SerializeField] private float jumpForce = 300f;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField] private ContactFilter2D filter2d;
+    private Vector2 inputDirection;
+    private PlayerInputProvider inputProvider;
+    private Rigidbody2D rb;
+
+    protected override void OnInitialize()
+    {
+        inputProvider = GetComponent<PlayerInputProvider>();
+        rb = GetComponent<Rigidbody2D>();
+
+        inputProvider.MoveDirection
+            //.Where(_ => CurrentGameState.Value == GameState.Battle)
+            .Subscribe(x =>
+            {
+                var value = Vector3.zero;
+                if (!IsAlive.Value) return;
+                if (IsGround.Value)
+                {
+                    value = x.normalized * runForce;
+                }
+                else
+                {
+					value = x.normalized * runForce * 0.5f;
+                }
+
+                inputDirection = value;
+            });
+
+        inputProvider.JumpButton
+            //.Where(_ => CurrentGameState.Value == GameState.Battle)
+            .Where(x => x && IsAlive.Value && IsGround.Value)
+            .ThrottleFirst(TimeSpan.FromSeconds(1))
+            .Subscribe(_ =>
+            {
+                Jump();
+            });
+
+        this.FixedUpdateAsObservable()
+                .Subscribe(_ =>
+                {
+                    Move();
+                    isGround.Value = CheckGrounded();
+                });
+
+
+        // inputProvider.MoveDirection
+        //         .Where(x => IsAlive.Value)
+        //         .Subscribe(x => _isRunning.Value = !IsJumping.Value && x.magnitude >= 0.1f);
+
+
+    }
+
+    private void Move()
+    {
+        float speedX = Mathf.Abs(this.rb.velocity.x);
+        if (speedX < runThreshold)
+        {
+            rb.AddForce(inputDirection);
+        }
+        else
+        {
+            this.transform.position += new Vector3(runSpeed * Time.deltaTime * inputDirection.normalized.x, 0, 0);
+        }
+    }
+
+    private void Jump()
+    {
+        rb.AddForce(transform.up * jumpForce);
+        isGround.Value = false;
+    }
+
+    private bool CheckGrounded()
+    {
+        return rb.IsTouching(filter2d);
+    }
 }
