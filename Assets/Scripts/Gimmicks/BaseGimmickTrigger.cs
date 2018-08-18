fb7204a3f5d9b0756523f3da3bda0e8dc67f8a4c
Takuma Seimiya <mj.soccer.t.s0802@gmail.com>
2018-08-18 13:12:57 +0900

    BaseGimmickの実装、DoTweenのインポート

---
+++ Assets/Scripts/Gimmicks/BaseGimmickTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+
+public class BaseGimmickTrigger : MonoBehaviour
+{
+    private ReactiveProperty<bool> onTrigger = new BoolReactiveProperty(false);
+    public IReadOnlyReactiveProperty<bool> OnTrigger { get { return onTrigger; } }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        onTrigger.Value = true;
+        GetComponent<BoxCollider2D>().enabled = false;
+    }
+}
