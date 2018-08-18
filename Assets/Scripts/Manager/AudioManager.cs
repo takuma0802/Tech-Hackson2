8c441cd4a16ae6e4e410ff5d52745b9cdcf5452f
Takuma Seimiya <mj.soccer.t.s0802@gmail.com>
2018-08-18 16:21:25 +0900

    Scene遷移の実装

---
+++ Assets/Scripts/Manager/AudioManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum AudioType
+{
+	TitleBGM = 0,
+	GameBGM= 1,
+	JumpSE= 2,
+	TogeSE= 3,
+	ExplosionSE= 4,
+}
+
+public class AudioManager : MonoBehaviour
+{
+
+	[SerializeField] AudioClip[] audioClips;
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+	public void PlayBGM(AudioType audio)
+	{
+		audioSource.clip = audioClips[(int)audio];
+		audioSource.Play();
+	}
+
+	public void StopBGM()
+	{
+		audioSource.clip = null;
+		audioSource.Stop();
+	}
+}
